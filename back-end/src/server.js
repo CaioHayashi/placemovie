4685eb5a569cb8190d2d52f6dd6b511c55b4d811
Caio Hayashi <caiohayashi81@gmail.com>
2024-11-02 15:30:07 -0300

    token criado

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,11 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes.js";
 import sessionRoutes from "./routes/session.routes.js";
-import supabase from "../db/supabase.js";
+import cors from "cors"
 
 dotenv.config();
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 app.use("/users", userRoutes);
 app.use("/login", sessionRoutes);
